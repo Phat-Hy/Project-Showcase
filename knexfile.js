c4581d6af675980ec39513014d7c48b41dccd7ee
Phat-Hy <hyphat2942004@gmail.com>
2026-08-01 18:35:39 +0700

    fix: define dev environment configuration in knexfile to support NODE_ENV=dev

--- knexfile.js
+++ knexfile.js
@@ -20,6 +20,26 @@ export default {
     }
   },
 
+  // Azure Dev Environment (Triggered when NODE_ENV=dev on Container Apps)
+  dev: {
+    client: 'pg',
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false }
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: './src/db/migrations',
+      tableName: 'knex_migrations'
+    },
+    seeds: {
+      directory: './src/db/seeds'
+    }
+  },
+
   staging: {
     client: 'pg',
     connection: {
