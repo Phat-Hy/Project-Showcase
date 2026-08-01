06f4c5d82eafc4dbcb11b68140f289751829b93b
Phat-Hy <hyphat2942004@gmail.com>
2026-08-01 17:42:42 +0700

    fix: change default location to eastus to ensure universal compatibility

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,7 +7,7 @@ variable "environment" {
 variable "location" {
   type        = string
   description = "The Azure region to deploy resources"
-  default     = "southeastasia"
+  default     = "eastus"
 }
 
 variable "project_name" {
